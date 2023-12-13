9d2893df140e24c10133116d4342a169c1dd8ff8
ines lucas <lucasinesmaria@gmail.com>
2023-12-13 17:47:08 +0000

    added Scroll Animation - incomplete

--- src/Experience.js
+++ src/Experience.js
@@ -1,6 +1,6 @@
 import { Suspense, useLayoutEffect, useRef } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
-import { SoftShadows, OrbitControls, useGLTF, useHelper, ScrollControls, useScroll } from '@react-three/drei';
+import { SoftShadows, OrbitControls, useGLTF, useHelper, ScrollControls, useScroll, Scroll } from '@react-three/drei';
 import { gsap } from "gsap";
 import { Perf } from 'r3f-perf';
 import { useControls } from 'leva';
@@ -11,117 +11,130 @@ import Toolbox from "./Toolbox.jsx";
 
 export default function Experience()
 {
-    {/** Experience is a component inside the Canvas - only place where we can use R3F hooks */}
+  {/** Experience is a component inside the Canvas - only place where we can use R3F hooks */}
 
-    const directionalLightRef = useRef();
-    useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, 'hotpink');
-    const { camera, gl } = useThree();
-    const { perfVisible, position, rotation } = useControls({
-      position:
-      {
-          value: { x: 0.3, y: 0.4, z: -1.7 },
-          step: 0.01,
-      },
-      rotation:
-      {
-          value: { x: 5, y: 0.02, z: 1.6 },
-          step: 0.001,
-      },
-      perfVisible: true,
-    })
+  const directionalLightRef = useRef();
+  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, 'hotpink');
+  const { camera, gl } = useThree();
+  const { perfVisible, position, rotation } = useControls({
+    position:
+    {
+        value: { x: 0.3, y: 0.4, z: -1.7 },
+        step: 0.01,
+    },
+    rotation:
+    {
+        value: { x: 5, y: 0.02, z: 1.6 },
+        step: 0.001,
+    },
+    perfVisible: true,
+  })
 
-    const groupAnimationRef = useRef();
-    const tl = useRef();
-    const scroll = useScroll();
-    const floorHeight = 2;
-    const numberOfFloors = 3;
+  const groupAnimationRef = useRef();
+  const tl = useRef();
+  const scroll = useScroll();
+  console.log("Scroll:", scroll); // returns null
+  // console.log("Scroll offset:", scroll.offset); // if uncommented, returns null and breaks website
 
-    useFrame( () => {
-      if (tl.current && scroll) {
-        tl.current.seek(scroll.offset * tl.current.duration());
-      }
-    });
+  useFrame((state, delta) => {
+      {/** useFrame is a hook that runs every frame. Add code that needs to run every frame, like animations. */}
 
-    useLayoutEffect(() => {
-      tl.current = gsap.timeline();
+    // Updating the animation based on the page's scroll position:
+    if (tl.current) { // Is GSAP timeline defined?
+      if (scroll) { // checks if the scroll position (from useScroll) is available.
+        if (scroll.offset !== tl.current.progress()) { // checks if the current scroll offset is different from the timeline's current progress
+          tl.current.seek(scroll.offset * tl.current.duration()); // seeks the timeline to a point that corresponds to the current scroll position. This means the timeline's progress is directly mapped to the scroll position.
+        }
+      };
+    };
+  });
 
-      // Vertical movement
-      tl.current.to(
-        groupAnimationRef.current.position,
-        {
-          duration: 2,
-          y: floorHeight * (numberOfFloors - 1), // positive value so it goes down (?)
-        },
-        0
-      );
+  {/** useLayoutEffect is a hook that runs after the DOM has been updated but before the browser has had a chance to "paint" those changes (the user doesn't see them yet). This makes it perfect for running animations. Use this instead of useEffect if you want to make changes to the DOM */}
+  useLayoutEffect(() => {
+    tl.current = gsap.timeline({defaults: { duration: 2, ease: 'power1.inOut' }});
 
-      tl.current.to(groupAnimationRef.current.position, { duration: 1, y: floorHeight }, "<");
+    tl.current // once page is fully loaded:
+    .to(groupAnimationRef.current.rotation, { y: 0.5 }, 0)
+    .to(groupAnimationRef.current.position, {y: 2}, 5)
+    .to(groupAnimationRef.current.position, {x: -1}, 5)
+    .to(groupAnimationRef.current.position, {z: -3}, 5)
+    .to(groupAnimationRef.current.position, {z: -2}, 6)
+  }, []);
 
-      tl.current.to(groupAnimationRef.current.position, { duration: 1, y: floorHeight * 2 }, "<");
-    }, []);
+  return <>
+    <ScrollControls pages={3} damping={0.25}>
+        {/* Toolbox and Objects inside */}
+      <group ref={ groupAnimationRef }>
+          {/** Essentials */}
+          <gridHelper args={[10, 10]} />
+          <axesHelper scale={ 5 } />
+          <SoftShadows size={ 80 } samples={ 20 } focus={ 0 } />
+          { perfVisible && <Perf position="top-left" /> }
+          <OrbitControls enableZoom={ false }/> {/* there's a makeDefault command - could be turned on */}
+          <directionalLight
+            ref={ directionalLightRef }
+            position={ [ 1, 3, 1.8]}
+            intensity={ 4 }
+            castShadow
+            shadow-mapSize={ [1024 * 3, 1024 * 3] }
+            shadow-camera-top={ 3 }
+            shadow-camera-right={ 3 }
+            shadow-camera-bottom={ -2 }
+            shadow-camera-left={ -2 }
+            shadow-camera-near={ 0.5 }
+            shadow-camera-far={ 50 }
+          />
+          <ambientLight intensity={ 1 } />
 
+        <group position-y={-0.6}>
+          <Suspense fallback={ null }>
+            <Thread scale={ 1 } rotation={ [0, 0.08, 1.6] } position={ [1.5, -0.18, -0.4] } />
+          </Suspense>
 
+          <Suspense fallback={ null }>
+            <Microphone scale={ 1 } rotation={ [rotation.x, rotation.y, rotation.z ] } position={ [ position.x, position.y, position.z ] }/>
+          </Suspense>
 
-    // useFrame((state, delta) => // state is inside useFrame but we don't want to get it at each reload. Only at 1st load to use for OrbitControls
-    // {
-    //     // console.log("tick", delta);
-    //     {/* delta = how much time has passed since last frame in seconds, can use it directly on rotation.y*/}
-    //     groupRef.current.rotation.y += delta * 0.5;
-    // });
+          <Suspense fallback={ null }>
+            <Toolbox scale={ 11 }/>
+          </Suspense>
+        </group>
 
-    return <>
-        {/** Essentials */}
-        <axesHelper scale={ 4 } />
-        <SoftShadows size={ 80 } samples={ 20 } focus={ 0 } />
-        { perfVisible && <Perf position="top-left" /> }
-        <OrbitControls enableZoom={ false }/>
-          {/* there's a makeDefault command - could be turned on */}
-        <directionalLight
-          ref={ directionalLightRef }
-          position={ [ 1, 3, 1.8]}
-          intensity={ 4 }
-          castShadow
-          shadow-mapSize={ [1024 * 2, 1024 * 2] }
-          shadow-camera-top={ 2 }
-          shadow-camera-right={ 3 }
-          shadow-camera-bottom={ -2 }
-          shadow-camera-left={ -2 }
-          shadow-camera-near={ 0.5 }
-          shadow-camera-far={ 50 }
-        />
-        <ambientLight intensity={ 1 } />
+        {/* Floor & Cube*/}
+        <group>
+          <mesh receiveShadow position-y={ -6.97 } scale={ 6 }>
+            <boxGeometry args={[1, 2, 1.5]}/> {/** width, height, depth */}
+            <meshPhysicalMaterial color="#654873" />
+          </mesh>
 
-        <ScrollControls pages={3} damping={0.25}>
-           {/* Toolbox and Objects inside */}
-          <group ref={ groupAnimationRef }>
-            <group position-y={-0.6}>
-              <Suspense fallback={ null }>
-                <Thread scale={ 1 } rotation={ [0, 0.08, 1.6] } position={ [1.5, -0.18, -0.4] } />
-              </Suspense>
+          {/* Temporary Placeholder */}
+          <mesh position={ [ 0, -2, 4.6] }>
+            <planeGeometry args={[6, 2]}/>
+            <meshStandardMaterial color="clear"/>
+          </mesh>
+        </group>
 
-              <Suspense fallback={ null }>
-                <Microphone scale={ 1 } rotation={ [rotation.x, rotation.y, rotation.z ] } position={ [ position.x, position.y, position.z ] }/>
-              </Suspense>
+        {/* Back Wall */}
+        <mesh position-z={- 4.5}>
+          <planeGeometry args={[6, 10]}/>
+          <meshStandardMaterial color="#654873"/>
+        </mesh>
+      </group>
 
-              <Suspense fallback={ null }>
-                <Toolbox scale={ 11 }/>
-              </Suspense>
-            </group>
-            {/* Floor */}
-            <mesh receiveShadow position-y={ -6.97 } scale={ 6 }>
-              <boxGeometry args={[1, 2, 1.5]}/>
-              <meshPhysicalMaterial color="#654873" />
-            </mesh>
-            {/* Back Wall */}
-            <mesh position-z={ -4.5}>
-              <planeGeometry args={[6, 10]}/>
-              <meshStandardMaterial color="#654873"/>
-            </mesh>
-          </group>
-        </ScrollControls>
-
-
-        {/* add an object that's color="mediumpurple" */}
-        {/** maybe bake the shadows as part of a sequence of movement? */}
-    </>
+      {/* Temporary Placeholder */}
+      <Scroll html style={{width: '100%'}}>
+        <h1 style={{ position: 'absolute', top: `132vh`}} >Scroll down</h1>
+      </Scroll>
+    </ScrollControls>
+  </>
 }
+
+// Notes
+{/* delta = how much time has passed since last frame in seconds, can use it directly on rotation.y*/}
+{/* add an object that's color="mediumpurple" */}
+{/** maybe bake the shadows as part of a sequence of movement? */}
+
+{/* seek() is a method that allows us to jump to a specific point in the animation. */}
+{/* offset is a value between 0 and 1 that represents how far down the page we've scrolled. */}
+{/* tl.current.duration() is the total duration of the animation. */}
+{/* tl.current.seek() is the current point in the animation. */}
