eead19518d565f9e20d145e2fed9b7fcd6563926
ines lucas <lucasinesmaria@gmail.com>
2023-12-14 17:47:50 +0000

    animation triggered with ScrollTrigger

---
+++ src/projectpages/EIFForOverlay.js
@@ -0,0 +1,10 @@
+import { ProjectContainer } from '../layout/styles.js';
+
+export default function EIFForOverlay() {
+  return <>
+    <ProjectContainer>
+      <h1>EIF for overlay</h1>
+      <p>This is information about my portfolio project at the European Investment Fund.</p>
+    </ProjectContainer>
+  </>;
+}
