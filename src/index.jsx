ac6d6c4deba7b8be756df2a9b4829afe17ddf921
ines lucas <lucasinesmaria@gmail.com>
2023-12-28 22:20:24 +0000

    loader

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import './style.css';
 import { Canvas } from '@react-three/fiber';
+import { Loader } from '@react-three/drei';
 import Experience from './Experience.js';
 import * as THREE from 'three';
 import styled from 'styled-components';
@@ -13,6 +14,22 @@ const ScrollContainer = styled.div`
 export default function Index() {
   const cameraRotation = [0, 1.3, 0]; // 0, 0, 0 - do not move X or Z
   const cameraPosition = [4, 2.5, 0]; // 0, 2, 6
+
+  const loaderFont = {
+    font: '/ABCMonumentGrotesk-Regular-Trial.woff',
+    fontSize: 30,
+    letterSpacing: 0,
+    lineHeight: 1,
+    color: 'white',
+    'materialToneMapped': false }
+  const loaderStyles = {
+    backgroundColor: '#654873',
+    color: 'white',
+  }
+  const barStyles = {
+    height: '10px', // Example: Thicker bar
+    width: '50%', // Example: Bar width relative to its container
+  };
 
   return <>
     <ScrollContainer>
@@ -35,7 +52,13 @@ export default function Index() {
       >
         <Experience />
       </Canvas>
+      {/* <Loader /> */}
     </ScrollContainer>
+    <Loader
+      dataStyles={loaderFont}
+      containerStyles={loaderStyles} // Flex layout styles
+      barStyles={barStyles} // Loading-bar styles
+      dataInterpolation={(p) => `${p.toFixed(2)}%`} />
 
     {/* Place here what's is needed to play */}
     {/* <EIFForOverlay/> */}
