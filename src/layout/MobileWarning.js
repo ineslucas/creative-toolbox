4092029113d8fb8cc40177e610329355035919e9
ines lucas <lucasinesmaria@gmail.com>
2024-01-09 17:04:34 +0000

    Speed insights, Mobile warning, readded work pages, about fixes

---
+++ src/layout/MobileWarning.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MessageContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 1.5em;
+  z-index: 9999;
+
+  @media (min-width: 971px) {
+    display: none; // Hide on larger screens
+  }
+
+  p {
+    padding-left: 10%;
+    padding-right: 10%;
+  }
+`;
+
+const MobileWarning = () => {
+  return (
+    <MessageContainer>
+      <p>Please check out Digipeasy on a computer for the best experience.</p>
+    </MessageContainer>
+  );
+}
+
+export default MobileWarning;
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,18 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const AboutMeContainer = styled.div`
-  // Basics
   background: #4C325F;
   height: 100vh;
   overflow: auto; // allows scrolling
 
-  padding: 15vh 10vw 10vh 2vw; // top right bottom left
+  padding: 20vh 2vw 10vh 2vw; // top right bottom left
     // creates automatic overflow
 
   h1 {
     font-family: 'ABCMonumentGrotesk-Regular-Trial', sans-serif;
     font-weight: 400;
-    font-size: 4em;
+    font-size: 3em;
     line-height: 1.2em;
     color: #fad9e4;
   }
@@ -22,20 +21,81 @@ const AboutMeContainer = styled.div`
     font-family: 'ABCMonumentGrotesk-Regular-Trial', sans-serif;
     font-weight: 400;
     font-size: 1.5em;
-    line-height: 1.2em;
+    line-height: 1.5em;
     color: #fad9e4;
+  }
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between; // Distributes space evenly
+  align-items: flex-start;
+  margin-top: 5vh;
+
+  @media (max-width: 768px) { // Tablet and mobile breakpoint
+    flex-direction: column;
+  }
+`;
+
+const FlexChild = styled.div`
+  flex: 1; /* so each child takes up equal width */
+  padding: 1em;
+
+  &:first-child {
+    padding-left: 0; // Remove left padding for the first child
+  }
+
+  &:last-child {
+    padding-right: 0; // Remove right padding for the last child
+  }
+
+  h1 {
+    font-family: 'ABCMonumentGrotesk-Regular-Trial', sans-serif;
+    font-weight: 400;
+    font-size: 2em;
+    line-height: 1.5em;
+    color: #fad9e4;
+  }
+
+  p {
+    font-family: 'ABCMonumentGrotesk-Regular-Trial', sans-serif;
+    color: #fad9e4;
+    font-weight: 400;
+    margin-bottom: 1em;
+    font-size: 1em;
+    line-height: 1.5;
   }
 `;
 
 const AboutMe = () => {
   return <>
     <AboutMeContainer>
-      <h1>About Me</h1>
-      <p>My name is Inês Lucas,
-        a Full Stack Developer and creative technologist based in Lisbon.
-        Entrepreneurship enthusiast previously at the European Investment Fund and Nestlé. Currently on a gap year.</p>
-      <p>My main goal is to create meaningful products that can make a difference in people's lives.  </p>
-      <p>I'm a big fan of the outdoors, photography and cooking.</p>
+      <p>Hi - my name is (Maria) Inês Lucas, a Marketeer turned Full Stack Developer based in Lisbon, Portugal.
+        Into entrepreneurship and creativity through technology.
+        Previously at the European Investment Fund and Nestlé. Currently on a gap year.</p>
+      <br />
+      <p>Digipeasy is the space acting as a playground for experimentation. My main goal is to create quality experiences for my community, which are web based for the moment.</p>
+
+      <FlexContainer>
+        <FlexChild>
+          <h1>the what where</h1>
+          <p>On a sabbatical, where I’ve recently finished an intensive Web Development bootcamp.
+            Previously, I was a Marketing Officer for the European Investment Fund.
+            I’ve lived in Luxembourg, Montréal, Oslo, Amsterdam, Thailand and Lisbon. Part-time digital nomad.</p>
+        </FlexChild>
+        <FlexChild>
+          <h1>the creative</h1>
+          <p>I’m working on building my Life Resumé as much as my creative one.
+            I enjoy creating, whether it’s websites, new digital designs or swimwear. </p>
+        </FlexChild>
+        <FlexChild>
+          <h1>the developer</h1>
+          <p>One of my first building experiences was my own startup for youth employment, helping students experiment with their career path:
+            Surf the Job in the Forward Pre-Acceleration Program.
+            Recently, since I’ve learnt how to code, I built GatherGo. Now working on Rails, React & Three JS projects like this portfolio which I coded and designed.</p>
+        </FlexChild>
+      </FlexContainer>
+
     </AboutMeContainer>
   </>
 }
