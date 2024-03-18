13cbcdf1d333e5a8d12c7a8c0c19d65d340ef1c4
ines lucas <lucasinesmaria@gmail.com>
2024-03-18 18:45:27 +0000

    updated images for project + added bounce animation to improve UX

--- src/pages/MemoryLab.js
+++ src/pages/MemoryLab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import Draggable from 'react-draggable';
 import { Link } from 'react-router-dom';
 import { PageContainer, Description, RightContainer, LeftContainer, BackButton, Title, TagsContainer, YearRoleTopicsTechTag, UpperRightLinkContainer } from './TemplateComponents/ProjectStyledTemplates';
@@ -9,6 +10,23 @@ const MemoryLab = () => {
     // Prevent the default drag image from being set
     event.preventDefault();
   };
+
+  const BouncyVertical = styled.div`
+    animation: bounce 2s ease-out;
+
+    @keyframes bounce {
+      0%   { transform: translateY(0); }
+      20%  { transform: translateY(-20px); }
+      40%  { transform: translateY(0); }
+      60%  { transform: translateY(-15px); }
+      80%  { transform: translateY(0); }
+      100% { transform: translateY(0); }
+    }
+  `;
+
+  const BouncyHorizontal = styled.div`
+    // to animate
+  `;
 
   return <>
     <Cursor />
@@ -38,19 +56,32 @@ const MemoryLab = () => {
             <YearRoleTopicsTechTag>Frontend Developer</YearRoleTopicsTechTag>
           </TagsContainer>
           <p>This project is a dynamic 3D visualization where users can upload their photography work. The main focus is on the interactive and dynamic nature of the 3D scene, with user-uploaded images being a key feature. I've always wanted a place on the inter-webs to showcase my photography. This is that.</p>
-          <br />
-          <p onClick={ () => window.open('https://github.com/ineslucas/photo_exhibit', '_blank')}>See font and icon credits on GitHub</p>
         </Description>
       </LeftContainer>
 
       <RightContainer>
-        {/* TBC animate these to be floating */}
-        <Draggable defaultPosition={{ x: 100, y: 400 }} onStart={handleStart}>
-          <img src="/images/memory_lab_wide.png" style={{ width: '300px', height: 'auto' }} />
-        </Draggable>
-        <Draggable defaultPosition={{ x: 380, y: 100 }} onStart={handleStart}>
-          <img src="/images/memory_lab_mobile.jpeg" style={{ width: '200px', height: 'auto' }} />
-        </Draggable>
+        {/* TBC animate to float */}
+        <BouncyVertical>
+          {/* Wide */}
+          <Draggable defaultPosition={{ x: 110, y: 450 }} onStart={handleStart}>
+            <img src="/images/memory_lab_wide.png" style={{ width: '300px', height: 'auto' }} />
+          </Draggable>
+
+          {/* Login */}
+          <Draggable defaultPosition={{ x: -250, y: -130 }} onStart={handleStart}>
+            <img src="/images/memory_lab_login.png" style={{ width: '200px', height: 'auto' }} />
+          </Draggable>
+
+          {/* Physics */}
+          <Draggable defaultPosition={{ x: 10, y: 320 }} onStart={handleStart}>
+            <img src="/images/memory_lab_physics.png" style={{ width: '200px', height: 'auto' }} />
+          </Draggable>
+
+          {/* Mobile */}
+          <Draggable defaultPosition={{ x: -15, y: 100 }} onStart={handleStart}>
+            <img src="/images/memory_lab_mobile.jpeg" style={{ width: '200px', height: 'auto' }} />
+          </Draggable>
+        </BouncyVertical>
       </RightContainer>
     </PageContainer>
   </>
