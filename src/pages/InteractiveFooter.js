af66daf09b6e6910a7443cc0c8a21979daa7d5c9
ines lucas <lucasinesmaria@gmail.com>
2024-04-06 23:59:43 +0100

    boundaries working

--- src/pages/InteractiveFooter.js
+++ src/pages/InteractiveFooter.js
@@ -1,5 +1,8 @@
+// Not in use. 
+// @react-three/rapier but trying to use the 2D components from @react-three/drei
+
 import { Canvas } from '@react-three/fiber';
-import { Box, Torus, Text } from "@react-three/drei";
+import { Box, Text } from "@react-three/drei";
 import React, { Suspense } from "react";
 import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
 import * as THREE from 'three';
@@ -25,25 +28,16 @@ const InteractiveFooter = () => {
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <Physics gravity={[0, -9.81, 0]}>
-        {/* Rectangle RigidBody */}
-        <RigidBody position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
-          <Box args={[2, 1, 0.1]} position={[0, 0, 0]}>
-            <meshStandardMaterial attach="material" color="royalblue" />
-            <Text fontSize={1} color="#f5a4bd">
-            {/* position={ [ 0, 0, 0.1]} */}
-              React
-            </Text>
-          </Box>
-          <CuboidCollider args={[2, 1, 0.1]} />
-        </RigidBody>
+
         {/* Floor */}
         <RigidBody type="fixed">
           <CuboidCollider args={[10, 0.1, 10]} position={[0, -2, 0]} />
           <mesh position={[0, -2, 0]}>
             <boxGeometry args={[10, 0.1, 10]} />
-            <meshStandardMaterial color="darkslategray" />
+            <meshStandardMaterial color="purple" />
           </mesh>
         </RigidBody>
+
       </Physics>
   </Canvas>
 }
