2988450e8aa2bd5fcbc5b5583e22c181d67393aa
ines lucas <lucasinesmaria@gmail.com>
2024-05-21 17:58:45 +0100

    quick fix, no material changes

--- src/layout/Overlay.js
+++ src/layout/Overlay.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { OverlayContainer, TopCenterBar, NavButton, NameDescription, AvatarImage, TopRight } from './styles';
+import { OverlayContainer, TopCenterBar, NavButton, NameDescription, AvatarImage } from './styles';
 // import About from './About.js';
 // import EIFForOverlay from '../pages/EIFForOverlay.js';
 
@@ -10,17 +10,16 @@ export default function Overlay()
   // const [showAbout, setShowAbout] = useState(false);
   // const toggleAbout = () => setShowAbout(!showAbout);
 
-  // TBD:
-  // function handleClick() {
-  //   // navigate to '/'
-  //   navigate('/');
+  function handleProjectsClick() {
+    // navigate to '/'
+    navigate('/');
 
-  //   // scroll to the top of the page
-  //   window.scrollTo({
-  //     top: 0,
-  //     behavior: 'smooth' // or 'auto' for instant scrolling
-  //   });
-  // }
+    // scroll to the top of the page
+    // window.scrollTo({
+    //   top: 0,
+    //   behavior: 'smooth' // or 'auto' for instant scrolling
+    // });
+  }
 
   return <>
     <OverlayContainer>
@@ -29,18 +28,13 @@ export default function Overlay()
             {/* onClick={() => navigate('/about')} */}
           <AvatarImage src="/images/about/purple_avatar.png"/>
             {/* onClick={() => navigate('/about')} */}
-          <NavButton onClick={() => navigate('/')}>Projects</NavButton>
+          <NavButton onClick={handleProjectsClick}>Projects</NavButton>
           {/* <NavButton>Playground</NavButton> */}
           {/* <NavButton>Digital Home</NavButton> */}
       </TopCenterBar>
 
       {/* { showAbout && <About/>} */}
       {/* <EIFForOverlay/> */}
-
-      {/* <TopRight> */}
-      {/* This top left should dissapear and become sticky to the bottom of the canvas */}
-      {/* Hi there, I’m <a href="https://mariaineslucas.com/" target="_blank">Inês’</a> creative toolbox, home to her creative projects. Pleasure to see you here. Each object means something - except the Leica. She sadly doesn’t own one. Drag the box and hover around. See what you can find. */}
-      {/* </TopRight> */}
 
     </OverlayContainer>
   </>
